c90d0b4d0c10bdf2a58475f05e87123ef83165f5
Frankie <dracko@hotmail.co.uk>
2020-07-13 15:34:37 +0000

    Script stop the table after 10 values

--- assets/js/roster.script.js
+++ assets/js/roster.script.js
@@ -1,16 +1,20 @@
 var url = "assets/js/roster.json";
-
+var page = 1
 
 $(document).ready(function() {
     $.getJSON(url, function(data) {
         var member_data = '';
-            $.each(data, function(key, value){
+            $.each(data, function(key, value) {
+
+                if (key <= 9) {
+                    console.log(key, value);
                 member_data += '<tr>';
                 member_data += '<td>'+value.name+'</td>';
                 member_data += '<td>'+value.level+'</td>';
                 member_data += '<td>'+value.role+'</td>';
                 member_data += '<td>'+value.rank+'</td>';
                 member_data += '</tr>';
+                }
             }); 
         const newLocal = '#members_table';
         $(newLocal).append(member_data);
