a6b097d075b4ef6dc06c30581d8ad8e28686b90a
Frankie <dracko@hotmail.co.uk>
2020-07-13 19:44:24 +0000

    1 to 10 & 11 to 20 members roster list on click event

--- assets/js/roster.script.js
+++ assets/js/roster.script.js
@@ -1,13 +1,31 @@
 var url = "assets/js/roster.json";
-var page = 1
 
-$(document).ready(function() {
+function numberOne() {
     $.getJSON(url, function(data) {
         var member_data = '';
-            $.each(data, function(key, value) {
+            $.each(data, function(i, value) {
+                if (i <= 9) {                  
+                    console.log(i);
+                member_data += '<tr>';
+                member_data += '<td>'+value.name+'</td>';
+                member_data += '<td>'+value.level+'</td>';
+                member_data += '<td>'+value.role+'</td>';
+                member_data += '<td>'+value.rank+'</td>';
+                member_data += '</tr>';
+                }; 
+            }); 
+        const newLocal = '#members_table';
+        $(newLocal).append(member_data);
+    });
+};
 
-                if (key <= 9) {
-                    console.log(key, value);
+document.getElementById("number-two").onclick = function() {
+    $.getJSON(url, function(data) {
+        var member_data = '';
+        var i;
+            $.each(data, function(i, value) {
+                if (i > 9 && i <= 19) {
+                    console.log(i);
                 member_data += '<tr>';
                 member_data += '<td>'+value.name+'</td>';
                 member_data += '<td>'+value.level+'</td>';
@@ -19,4 +37,5 @@ $(document).ready(function() {
         const newLocal = '#members_table';
         $(newLocal).append(member_data);
     });
-});
+};
+
