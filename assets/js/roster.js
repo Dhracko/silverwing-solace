0fb89c1c21700dbf23c4da371b227199b245717e
Frankie <dracko@hotmail.co.uk>
2020-07-16 16:22:01 +0000

    Google map API added with 3 locations

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -15,7 +15,7 @@ function getData(type, cb) {
     xhr.send();
 }
 
-$(document).ready(function() {
+$(document).ready(function() { //loads the first 10 members when page finish loading
     $.getJSON(url, function(data) {
         var member_roster = "";
         for (i=0; i <= 9; i++) {
@@ -35,7 +35,7 @@ function writeToDocument(type) {
     var el = document.getElementById("roster");
     el.innerHTML = '<tr>' + '<td>Character name </td>' +'<td>Level</td>'+'<td>Role</td>'+'<td>Guild Rank</td>'+'</tr>';
     var i;
-    if (type == "pageOne") {
+    if (type == "pageOne") { // Check which button page the users has pressed
         var i = 0;
         var end = 9;
     } else if (type == "pageTwo") {
@@ -51,34 +51,36 @@ function writeToDocument(type) {
         var i = 40;
         var end = 49;
     };
-    getData(type, function(item) {
+    getData(type, function(item) { //Add the table rows and the table to the right page
         for (i; i <= end; i++) {
             el.innerHTML += '<tr>' + '<td>' + item[i].name + '</td>' +'<td>'+  item[i].level+ '</td>'+'<td>' + item[i].role + '</td>'+'<td>' + item[i].rank + '</td>'+'</tr>';
         };
     });
 };
 
-// Google Map API
-
+//Google Maps API
 
 function initMap() {
-
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 2,
         center: { lat: 16.619261, lng: -13.134766 }
     });
 
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var markers = locations.map(function (location, i) {
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+
+    var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
-    })
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      }
-        var locations = [
-        {lat: -31.563910, lng: 147.154312}
-        ]
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
+      
