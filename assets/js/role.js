e077bbdce2f0453e7f7e0054329613bb337c77c2
Frankie <dracko@hotmail.co.uk>
2020-07-18 10:59:01 +0000

    Deleted main.js, roster.js and copy the objects into role.js from members.json

--- assets/js/role.js
+++ assets/js/role.js
@@ -1,13 +1,97 @@
-var url = "assets/js/members.Json";
+var members = [{
+    "level": 120,
+    "location": {
+      "South Africa": [-34.2969541, 18.2479026]
+    },
+    "name": "Lexari",
+    "rank": 0,
+    "role": "DPS"
+  },
+  {
+    "level": 120,
+    "location": {
+      "Egypt": [26.844829, 26.3840401]
+    },
+    "name": "Mapen",
+    "rank": 2,
+    "role": "Tank"
+  },
+  {
+    "level": 120,
+    "location": {
+      "South Africa": [-34.2969541, 18.2479026]
+    },
+    "name": "Aysira",
+    "rank": 2,
+    "role": "Healer"
+  },
+  {
+    "level": 120,
+    "location": {
+      "Germany": [51.0968582, 5.9690268]
+    },
+    "name": "Bentlee",
+    "rank": 2,
+    "role": "DPS"
+  },
+  {
+    "level": 120,
+    "location": {
+      "Belgium": [50.4974444, 3.345157]
+    },
+    "name": "Deriki",
+    "rank": 2,
+    "role": "Tank"
+  },
+  {
+    "level": 120,
+    "location": {
+      "Turkey": [39.0100679, 30.6885503]
+    },
+    "name": "Refaralon",
+    "rank": 2,
+    "role": "DPS"
+  },
+  {
+    "level": 120,
+    "location": {
+      "Portugal": [37.1458903, -23.3168015]
+    },
+    "name": "Berzak",
+    "rank": 2,
+    "role": "Healer"
+  },
+  {
+    "level": 39,
+    "location": {
+      "Spain": [40.1301544, -8.201855]
+    },
+    "name": "Locketo",
+    "rank": 2,
+    "role": "DPS"
+  },
+  {
+    "level": 120,
+    "location": {
+      "France": [46.139033, -2.4351197]
+    },
+    "name": "Yirinae",
+    "rank": 3,
+    "role": "Tank"
+  },
+  {
+    "level": 120,
+    "location": {
+      "England": [52.7602507, -6.8123259]
+    },
+    "name": "Theomis",
+    "rank": 6,
+    "role": "Healer"
+  }
+];
 
-$(document).ready(function() { //loads the members when page finish loading
-    $.getJSON(url, function(data) {
-        var tank = "";
-        member_role="";   
-        member_roster += data[i].name;    
-        member_role += data[i].role;    
-        console.log(member_role)    
-        $('#roster').append(member_roster);
-    });
-        
-});
+
+
+
+
+
